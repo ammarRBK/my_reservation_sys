c5dd45a62caf5af5de960b095810022d37c52a0c
ammarRBK <alshakmaskeain@gmail.com>
2024-05-17 11:52:48 +0300

    add login page with style and get data from it’s form

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton } from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton, IonInput, IonHeader, IonToolbar, IonTitle, IonContent, FormsModule],
 })
 export class HomePage {
+  username!: string;
+  password!: string;
   constructor() {}
+  
+  login(){
+    console.log(this.username, this.password)
+  }
 }
