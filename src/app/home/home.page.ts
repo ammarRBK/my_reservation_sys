38ad8bbfeafaa40407bcf107c794e1a578f16be2
ammarRBK <alshakmaskeain@gmail.com>
2024-05-19 09:56:25 +0300

    add auth messages

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton, IonItem } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton, IonItem, IonText } from '@ionic/angular/standalone';
 
 
 @Component({
@@ -9,22 +9,31 @@ import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton, IonIt
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonItem, IonButton, IonInput, IonHeader, IonToolbar, IonTitle, IonContent, FormsModule],
+  imports: [IonText, IonItem, IonButton, IonInput, IonHeader, IonToolbar, IonTitle, IonContent, FormsModule],
 })
 export class HomePage {
   username!: string;
   password!: string;
+  errorMessage: string='';
   constructor(private router: Router) {}
   
   login(){ 
       if('OdaiWaMahmoud' === this.username){
         if(this.password == 'U2PWp*eMQ8VC5JT'){
-          this.router.navigateByUrl('reservsyslogin')
+          this.router.navigateByUrl('reservsyslogin');
         }else{
+          this.errorMessage= "كلمة سر خاطئة";
+          setTimeout(() => {
+            this.errorMessage= "";
+          }, 3000);
           console.log("wrong password");
         }
       }else{
+        this.errorMessage= "اسم مستخدم خاطئ";
         console.log('wrong username');
+        setTimeout(() => {
+          this.errorMessage= "";
+        }, 3000);
       }
   }
 }
